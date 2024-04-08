322bbbe3dd84de15995c94908eefd4180e2135e2
Ilgiz-RAM <162990410+Ilgiz-RAM@users.noreply.github.com>
2024-04-08 11:50:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwqPzhP4EnZXhQp_uVcLvz_P2Ih1LoFqU",
+  authDomain: "myfinalexamfirebaselogin-f9e52.firebaseapp.com",
+  projectId: "myfinalexamfirebaselogin-f9e52",
+  storageBucket: "myfinalexamfirebaselogin-f9e52.appspot.com",
+  messagingSenderId: "573608188999",
+  appId: "1:573608188999:web:9d771bbf13e9ec09156562"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+// inputs
+
+const name = document.getElementById(username).value;
+const telephone = document.getElementById(telephone).value;
+
+// submit button
+
+
+
+const submit = document.getElementById(submit);
+submit.addEventListener("click", function(event){
+    event.preventDefault()
+    alert(5)
+})
+
+
+
+// I gave up,sorry,i tried My best
+//                          (Ilgiz)
